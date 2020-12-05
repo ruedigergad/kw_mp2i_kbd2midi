f5bf004d6c7d3b747a8eb153baf508d52bc6228e
Ruediger Gad <r.c.g@gmx.de>
2020-12-05 19:59:06 +0100

    Use proper pin connection.

--- kw_mp2i_kbd2midi.ino
+++ kw_mp2i_kbd2midi.ino
@@ -1,4 +1,4 @@
-#define NOT_MUTE 2
+#define NOT_MUTE 53
 
 void setup() {
   Serial.begin(9600);
@@ -8,15 +8,19 @@ void setup() {
   pinMode(NOT_MUTE, OUTPUT);
   digitalWrite(NOT_MUTE, HIGH);
 
+  for (int i = 3; i <= 10; i++) {
+    pinMode(i, INPUT_PULLUP);
+  }
+
   // Left "half" of keyboard (Bass).
   for (int i = 23; i <= 41; i += 2) {
     pinMode(i, OUTPUT);
-    digitalWrite(i, LOW);
+    digitalWrite(i, HIGH);
   }
   // Right "half" of keyboard (Treble).
   for (int i = 22; i <= 44; i += 2) {
     pinMode(i, OUTPUT);
-    digitalWrite(i, LOW);
+    digitalWrite(i, HIGH);
   }
 }
 
@@ -24,40 +28,36 @@ void loop() {
   Serial.print("B: ");
   for (int i = 0; i <= 9; i++) {
     int line = 23 + (2 * i);
-    digitalWrite(line, HIGH);
+    digitalWrite(line, LOW);
 
     Serial.print(i);
     Serial.print(") ");
     byte key_press_b = 0;
     for (byte j = 0; j <= 7; j++) {
-      int raw = analogRead(54 + j);
-      byte tmp = raw > 900 ? 1 : 0;
-      key_press_b += (tmp << j);
+      int raw = !digitalRead(3 + j);
+      key_press_b += (raw << j);
     }
-    digitalWrite(line, LOW);
-    delay(3);
+    digitalWrite(line, HIGH);
     
     Serial.print(key_press_b);
     Serial.print(" ");
   }
 
-  Serial.print("    T: ");
+  Serial.print("   T: ");
   for (int i = 0; i <= 11; i++) {
     int line = 22 + (2 * i);
-    digitalWrite(line, HIGH);
+    digitalWrite(line, LOW);
 
     Serial.print(i);
     Serial.print(") ");
-    byte key_press_b = 0;
+    byte key_press_t = 0;
     for (byte j = 0; j <= 7; j++) {
-      int raw = analogRead(54 + j);
-      byte tmp = raw > 900 ? 1 : 0;
-      key_press_b += (tmp << j);
+      int raw = !digitalRead(3 + j);
+      key_press_t += (raw << j);
     }
-    digitalWrite(line, LOW);
-    delay(3);
+    digitalWrite(line, HIGH);
     
-    Serial.print(key_press_b);
+    Serial.print(key_press_t);
     Serial.print(" ");
   }
   
