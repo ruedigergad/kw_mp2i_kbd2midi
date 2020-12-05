6a02340e0f48c24d2307b955a9a85dd195900d50
Ruediger Gad <r.c.g@gmx.de>
2020-12-05 17:51:16 +0100

    Share analog in use different digital out.

--- kw_mp2i_kbd2midi.ino
+++ kw_mp2i_kbd2midi.ino
@@ -9,23 +9,25 @@ void setup() {
   digitalWrite(NOT_MUTE, HIGH);
 
   // Left "half" of keyboard (Bass).
-  for (int i = 23; i <= 33; i++) {
+  for (int i = 23; i <= 41; i += 2) {
     pinMode(i, OUTPUT);
     digitalWrite(i, LOW);
   }
-  /*
-  for (int i = 39; i <= 53; i += 2) {
-    pinMode(i, INPUT);
+  // Right "half" of keyboard (Treble).
+  for (int i = 22; i <= 44; i += 2) {
+    pinMode(i, OUTPUT);
+    digitalWrite(i, LOW);
   }
-  */
 }
 
 void loop() {
-  for (int i = 23; i <= 33; i++) {
-    digitalWrite(i, HIGH);
+  Serial.print("B: ");
+  for (int i = 0; i <= 9; i++) {
+    int line = 23 + (2 * i);
+    digitalWrite(line, HIGH);
 
     Serial.print(i);
-    Serial.print(") B:");
+    Serial.print(") ");
     byte key_press_b = 0;
     for (byte j = 0; j <= 7; j++) {
       int raw = analogRead(54 + j);
@@ -33,19 +35,29 @@ void loop() {
       key_press_b += (tmp << j);
     }
     Serial.print(key_press_b);
+    Serial.print(" ");
     
-    Serial.print(" T:");
-    byte key_press_t = 0;
-    for (byte j = 0; j <= 7; j++) {
-      int raw = analogRead(62 + j);
-      byte tmp = raw > 900 ? 1 : 0;
-      key_press_t += (tmp << j);
-    }
-    Serial.print(key_press_t);
-    
-    digitalWrite(i, LOW);
-
-    Serial.print("   ");
+    digitalWrite(line, LOW);
   }
+
+  Serial.print("    T: ");
+  for (int i = 0; i <= 11; i++) {
+    int line = 22 + (2 * i);
+    digitalWrite(line, HIGH);
+
+    Serial.print(i);
+    Serial.print(") ");
+    byte key_press_b = 0;
+    for (byte j = 0; j <= 7; j++) {
+      int raw = analogRead(54 + j);
+      byte tmp = raw > 900 ? 1 : 0;
+      key_press_b += (tmp << j);
+    }
+    Serial.print(key_press_b);
+    Serial.print(" ");
+    
+    digitalWrite(line, LOW);
+  }
+  
   Serial.print("\n");
 }
