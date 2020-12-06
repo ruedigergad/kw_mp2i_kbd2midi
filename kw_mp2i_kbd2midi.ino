fdc4692644892c5a8cbe3e83da4ab2e10a813a44
Ruediger Gad <r.c.g@gmx.de>
2020-12-06 10:20:13 +0100

    Intermediate commit, working via ttymidi: https://github.com/cjbarnes18/ttymidi

--- kw_mp2i_kbd2midi.ino
+++ kw_mp2i_kbd2midi.ino
@@ -1,7 +1,23 @@
+#include <MIDI.h>
+
 #define NOT_MUTE 53
 
+/*
+struct MySettings : public midi::DefaultSettings
+{
+   static const uint16_t SenderActiveSensingPeriodicity = 0;
+   static const bool UseRunningStatus = false;
+   static const bool UseSenderActiveSensing = false;
+   static const long BaudRate = 31250;
+};
+*/
+//MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial3, MIDI, MySettings);
+//MIDI_CREATE_INSTANCE(HardwareSerial, Serial3, MIDI);
+MIDI_CREATE_DEFAULT_INSTANCE();
+
 void setup() {
-  Serial.begin(9600);
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+  Serial.begin(115200);
   
   pinMode(LED_BUILTIN, OUTPUT);
 
@@ -25,13 +41,14 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("B: ");
+  //MIDI.sendActiveSensing();
+  //Serial.print("B: ");
   for (int i = 0; i <= 9; i++) {
     int line = 23 + (2 * i);
     digitalWrite(line, LOW);
 
-    Serial.print(i);
-    Serial.print(") ");
+    //Serial.print(i);
+    //Serial.print(") ");
     byte key_press_b = 0;
     for (byte j = 0; j <= 7; j++) {
       int raw = !digitalRead(3 + j);
@@ -39,17 +56,25 @@ void loop() {
     }
     digitalWrite(line, HIGH);
     
-    Serial.print(key_press_b);
-    Serial.print(" ");
+    //Serial.print(key_press_b);
+
+    if (key_press_b != 0) {
+      MIDI.sendNoteOn(42, 127, 1);
+      delay(10);
+      MIDI.sendNoteOff(42, 127, 1);
+      //delay(1000);
+    }
+    
+    //Serial.print(" ");
   }
 
-  Serial.print("   T: ");
+  //Serial.print("   T: ");
   for (int i = 0; i <= 11; i++) {
     int line = 22 + (2 * i);
     digitalWrite(line, LOW);
 
-    Serial.print(i);
-    Serial.print(") ");
+    //Serial.print(i);
+    //Serial.print(") ");
     byte key_press_t = 0;
     for (byte j = 0; j <= 7; j++) {
       int raw = !digitalRead(3 + j);
@@ -57,9 +82,10 @@ void loop() {
     }
     digitalWrite(line, HIGH);
     
-    Serial.print(key_press_t);
-    Serial.print(" ");
+    //Serial.print(key_press_t);
+    //Serial.print(" ");
   }
   
-  Serial.print("\n");
+  //Serial.print("\n");
+  delay(100);
 }
